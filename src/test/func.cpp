834f5909e5f2a7971f0e96e2868567dee6e3bb2a
rockcutter <rockcutter414@gmail.com>
2024-01-23 20:07:34 +0000

    [fix] fix testcase

--- src/test/func.cpp
+++ src/test/func.cpp
@@ -3,11 +3,9 @@
 #include <gtest/gtest.h>
 
 TEST(FUNC_TEST, SUCCESS_EXPECTS_AND_ENSURES){
-    ASSERT_DEATH({
-            threshold_gt_20_expects_ensures(21, 21);
-        },
-        ""
-    );
+    ASSERT_EQ(21, threshold_gt_20_expects_ensures(21, 21));
+    ASSERT_EQ(100, threshold_gt_20_expects_ensures(21, 100));
+    ASSERT_EQ(200, threshold_gt_20_expects_ensures(21, 200));
 }
 
 TEST(FUNC_TEST, FAIL_EXPECTS){
@@ -16,11 +14,36 @@ TEST(FUNC_TEST, FAIL_EXPECTS){
         },
         ""
     );
+    ASSERT_DEATH({
+            threshold_gt_20_expects_ensures(10, 21);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            threshold_gt_20_expects_ensures(0, 21);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            threshold_gt_20_expects_ensures(-1, 21);
+        },
+        ""
+    );
 }
 
 TEST(FUNC_TEST, FAIL_ENSURES){
     ASSERT_DEATH({
             threshold_gt_20_expects_ensures(21, 20);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            threshold_gt_20_expects_ensures(21, 0);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            threshold_gt_20_expects_ensures(21, -1);
         },
         ""
     );
