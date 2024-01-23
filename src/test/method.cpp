b269721d6c232ef6aa3c33624dcec81689fafac0
rockcutter <rockcutter414@gmail.com>
2024-01-23 20:07:25 +0000

    [fix] fix testcase

--- src/test/method.cpp
+++ src/test/method.cpp
@@ -3,11 +3,9 @@
 
 TEST(METHOD_TEST, SUCCESS_EXPECTS_AND_ENSURES){
     Hoge hoge;
-    ASSERT_DEATH({
-            hoge.threshold_gt_20_expects_ensures(21, 21);
-        },
-        ""
-    );
+    ASSERT_EQ(21, hoge.threshold_gt_20_expects_ensures(21, 21));
+    ASSERT_EQ(100, hoge.threshold_gt_20_expects_ensures(21, 100));
+    ASSERT_EQ(200, hoge.threshold_gt_20_expects_ensures(21, 200));
 }
 
 TEST(METHOD_TEST, FAIL_EXPECTS){
@@ -17,12 +15,37 @@ TEST(METHOD_TEST, FAIL_EXPECTS){
         },
         ""
     );
+    ASSERT_DEATH({
+            hoge.threshold_gt_20_expects_ensures(10, 21);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            hoge.threshold_gt_20_expects_ensures(0, 21);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            hoge.threshold_gt_20_expects_ensures(-1, 21);
+        },
+        ""
+    );
 }
 
 TEST(METHOD_TEST, FAIL_ENSURES){
     Hoge hoge;
     ASSERT_DEATH({
             hoge.threshold_gt_20_expects_ensures(21, 20);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            hoge.threshold_gt_20_expects_ensures(21, 0);
+        },
+        ""
+    );
+    ASSERT_DEATH({
+            hoge.threshold_gt_20_expects_ensures(21, -1);
         },
         ""
     );
